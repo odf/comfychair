8a3b706ef65e7b55b316c3cf8d216c695633febf
odf <olaf.delgado@gmail.com>
2014-03-02 19:20:11 +1100

    add the basice framework code, a Jasmine helper and some tests

---
+++ jasmine.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var G = require('./index');
+
+
+var customMatchers = {
+  toSucceedOn: function(generator, shrinker) {
+    var result = G.check(this.actual, generator, shrinker);
+    this.message = function() { return result.cause; };
+    return result.successful;
+  },
+  toConformTo: function(model) {
+    var result = G.checkSystem(this.actual, model);
+    this.message = function() { return result.cause; };
+    return result.successful;
+  }
+};
+
+beforeEach(function() { this.addMatchers(customMatchers) });
